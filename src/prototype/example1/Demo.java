40d0fb839910531a0b639d654227fba391fcf551
Amir <amirkenesbay@gmail.com>
2021-12-15 01:50:34 +0600

    Add prototype patternt

---
+++ src/prototype/example1/Demo.java
@@ -0,0 +1,51 @@
+package prototype.example1;
+
+import prototype.example1.shapes.Circle;
+import prototype.example1.shapes.Rectangle;
+import prototype.example1.shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        rectangle.color = "blue";
+        shapes.add(rectangle);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+
+    public static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy){
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+        for (int i = 0; i < shapes.size(); i++) {
+            if(shapes.get(i) != shapesCopy.get(i)){
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if(shapes.get(i).equals(shapesCopy.get(i))){
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": And they are identical (yay!)");
+                }
+            } else {
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+    }
+}
