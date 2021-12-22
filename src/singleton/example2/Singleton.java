a5054e42a3310306b14d0d5086e578724e9f8508
Amir <amirkenesbay@gmail.com>
2021-12-23 02:10:09 +0600

    Add second example of singleton

--- src/singleton/example2/Singleton.java
+++ src/singleton/example2/Singleton.java
@@ -7,20 +7,30 @@ public class Singleton {
 
     public String value;
 
-    private Singleton(String value){
+    private Singleton(String value) {
         this.value = value;
     }
 
-//    public static Singleton getInstance(String value){
-//        // Техника, которую мы здесь применяем называется «блокировка с двойной
-//        // проверкой» (Double-Checked Locking). Она применяется, чтобы
-//        // предотвратить создание нескольких объектов-одиночек, если метод будет
-//        // вызван из нескольких потоков одновременно.
-//        //
-//        // Хотя переменная `result` вполне оправданно кажется здесь лишней, она
-//        // помогает избежать подводных камней реализации DCL в Java.
-//        //
-//        // Больше об этой проблеме можно почитать здесь:
-//        // https://refactoring.guru/ru/java-dcl-issue
-//    }
+    public static Singleton getInstance(String value) {
+        // Техника, которую мы здесь применяем называется «блокировка с двойной
+        // проверкой» (Double-Checked Locking). Она применяется, чтобы
+        // предотвратить создание нескольких объектов-одиночек, если метод будет
+        // вызван из нескольких потоков одновременно.
+        //
+        // Хотя переменная `result` вполне оправданно кажется здесь лишней, она
+        // помогает избежать подводных камней реализации DCL в Java.
+        //
+        // Больше об этой проблеме можно почитать здесь:
+        // https://refactoring.guru/ru/java-dcl-issue
+        Singleton result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized (Singleton.class) {
+            if (instance == null) {
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
+    }
 }
